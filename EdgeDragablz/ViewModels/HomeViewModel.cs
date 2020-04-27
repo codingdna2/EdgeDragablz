b0162b2ed24a0e5cd2f066b9750424168334cf5c
Dan <codingdna@gmail.com>
2020-04-27 19:01:55 +0200

    Use CanvasItemsControlStyle as UniformGridItemsControlStyle kills drag&drop

--- EdgeDragablz/ViewModels/HomeViewModel.cs
+++ EdgeDragablz/ViewModels/HomeViewModel.cs
@@ -2,6 +2,6 @@
 {
     public class HomeViewModel : DocumentViewModel
     {
-        public override string Header => "New Tab 1234567890 1234567890";
+        public override string Header => "New Tab";
     }
 }

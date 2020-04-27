8f78ddb81aa1f54556a520ec2eec93d44b7d65b3
Dan <codingdna@gmail.com>
2020-04-27 23:05:32 +0200

    Cleanup

--- EdgeDragablz/MainWindowViewModel.cs
+++ EdgeDragablz/MainWindowViewModel.cs
@@ -2,14 +2,16 @@
 using Dragablz;
 using EdgeDragablz.Views;
 using Prism.Commands;
+using Prism.Mvvm;
 using Prism.Regions;
 using System;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Windows.Input;
 
 namespace EdgeDragablz
 {
-    public class MainWindowViewModel
+    public class MainWindowViewModel : BindableBase
     {
         private readonly IRegionManager regionManager;
 
@@ -29,8 +31,6 @@ namespace EdgeDragablz
         public ICommand AddCommand { get; }
 
         public ICommand CloseAllCommand { get; }
-
-        public static Func<HomeView> Factory => () => ServiceLocator.Current.GetInstance<HomeView>();
 
         private void Navigate(string navigatePath)
         {
